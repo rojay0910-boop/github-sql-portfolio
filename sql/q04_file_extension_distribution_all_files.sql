5507ff0fe3c3b633e865c03b304f5b3cb303c6c9
rojay0910-boop <rojay0910@gmail.com>
2025-12-28 08:13:36 +0900

    Update q04_file_extension_distribution_all_files.sql

--- sql/q04_file_extension_distribution_all_files.sql
+++ sql/q04_file_extension_distribution_all_files.sql
@@ -1,6 +1,6 @@
 -- q04_file_extension_distribution
 -- Goal: Count files by extension (simple version).
--- Uses the smaller sample table to keep costs low.
+-- (using the low-cost sample table)
 
 SELECT
   LOWER(REGEXP_EXTRACT(path, r'\.([^.\/]+)$')) AS extension,
